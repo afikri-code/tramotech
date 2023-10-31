9b1737b18e7e50f7c9bdb69b51ed964356784e6f
Ahmed Fikri <github@afikri.de>
2023-10-31 10:59:59 +0100

    new exercises added

--- kotlin/kotlin-katas/src/main/kotlin/Katas.kt
+++ kotlin/kotlin-katas/src/main/kotlin/Katas.kt
@@ -1,3 +1,4 @@
+data class Person(val name: String, val age: Int)
 class Katas {
     fun sumOfEvens(numbers: List<Int>): Int {
         return 0
@@ -10,5 +11,21 @@ class Katas {
     // Function to find the maximum number in a list
     fun findMaxNumber(numbers: List<Int>): Int? {
         return 0
+    }
+
+    fun filterAndTransform(strings: List<String>): List<String>? {
+        return listOf("")
+    }
+
+    fun calculateAverageOfEvens(numbers: List<Int>): Double {
+        return 0.0
+    }
+
+    fun findDuplicates(numbers: List<Int>): List<Int>? {
+        return listOf(0)
+    }
+
+    fun sortAndFilterPeople(people: List<Person>): List<Person>? {
+        return people
     }
 }
--- kotlin/kotlin-katas/src/test/kotlin/KatasKtTest.kt
+++ kotlin/kotlin-katas/src/test/kotlin/KatasKtTest.kt
@@ -34,5 +34,34 @@ class KatasKtTest {
         assertEquals(10,result)
     }
 
+    @Test
+    fun testFilterAndTransform() {
+        val strings = listOf("apple", "banana", "cherry", "date")
+        val result = katas.filterAndTransform(strings)
+        assertEquals(listOf("APPLE", "BANANA", "DATE"), result)
+    }
+
+    @Test
+    fun testCalculateAverageOfEvens() {
+        val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        val result = katas.calculateAverageOfEvens(numbers)
+        assertEquals(6.0, result, 0.01)
+    }
+
+    @Test
+    fun testFindDuplicates() {
+        val numbers = listOf(1, 2, 2, 3, 4, 4, 4, 5, 5, 6)
+        val result = katas.findDuplicates(numbers)
+        assertEquals(listOf(2, 4, 5), result)
+    }
+
+    @Test
+    fun testSortAndFilterPeople() {
+        val people = listOf(Person("Alice", 28), Person("Bob", 35), Person("Charlie", 22), Person("David", 40))
+        val result = katas.sortAndFilterPeople(people)
+        val expected = listOf(Person("Bob", 35), Person("David", 40))
+        assertEquals(expected, result)
+    }
+
 }
 
